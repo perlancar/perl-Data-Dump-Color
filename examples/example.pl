76e49a19437cffe79752d639ddbba0b65884c788
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-11-13 12:40:48 +0700

    Add some more examples to demonstrate alignment bugs

--- examples/example.pl
+++ examples/example.pl
@@ -31,12 +31,24 @@ dd({
     array_empty => [],
     array_long => [qw/For backward compatibility with older implementations that
                       didn't support anonymous globs/],
+    array_aoh_mostly => [
+        { name => 'Andi', dob => '1988-10-10', employee_id=>1, some_other_attribute => 'some value', },
+        { name => 'Budi', dob => '1983-01-22', employee_id=>2, some_other_attribute => 'some value', },
+        { name => 'Cika', dob => '1986-07-03', employee_id=>3, some_other_attribute => 'some value', },
+        "this one is not a hash",
+    ],
 
     # hashes
     hash => {a=>1, b=>2, c=>[], d=>{}, e=>undef},
     hash_empty => {},
     hash_long => {qw/For backward compatibility with older implementations that
                      didn't support anonymous globs/},
+    hash_hoh_mostly => {
+        EMP001 => { name => 'Andi', dob => '1988-10-10', employee_id=>'EMP001', some_other_attribute => 'some value', },
+        EMP002 => { name => 'Budi', dob => '1983-01-22', employee_id=>'EMP003', some_other_attribute => 'some value', },
+        EMP3   => { name => 'Cika', dob => '1986-07-03', employee_id=>'EMP3'  , some_other_attribute => 'some value', },
+        key4   => "this one is not a hash",
+    },
 
     # objects
     obj => bless({a=>1, b=>2}, "Foo"),
